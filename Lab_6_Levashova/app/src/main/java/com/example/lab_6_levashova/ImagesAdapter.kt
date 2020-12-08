e60ed800c1c48db473f59c3d308bf61d4c26110d
tommmatojuice <62665598+tommmatojuice@users.noreply.github.com>
2020-12-09 00:01:17 +0300

    finaly version

--- Lab_6_Levashova/app/src/main/java/com/example/lab_6_levashova/ImagesAdapter.kt
+++ Lab_6_Levashova/app/src/main/java/com/example/lab_6_levashova/ImagesAdapter.kt
@@ -5,16 +5,19 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
 import coil.transform.CircleCropTransformation
 
-class ImagesAdapter(context: Context): RecyclerView.Adapter<ImagesAdapter.ViewHolder>()
+class ImagesAdapter(context: Context,
+                    private val images: MutableList<ImageResponse>): RecyclerView.Adapter<ImagesAdapter.ViewHolder>()
 {
     private val inflater: LayoutInflater = LayoutInflater.from(context)
-    private val images: MutableList<ImageResponse> = mutableListOf()
+//    private val images: MutableList<ImageResponse> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(inflater.inflate(R.layout.item_image, parent, false))
@@ -40,7 +43,8 @@ class ImagesAdapter(context: Context): RecyclerView.Adapter<ImagesAdapter.ViewHo
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         private val image: ImageView = itemView.findViewById(R.id.image)
-        private val title: TextView = itemView.findViewById(R.id.image_text)
+        private val positionText: TextView = itemView.findViewById(R.id.image_position)
+        private val author: TextView = itemView.findViewById(R.id.image_author)
 
         fun bind(version: ImageResponse, position: Int) {
             image.load(version.download_url){
@@ -48,7 +52,8 @@ class ImagesAdapter(context: Context): RecyclerView.Adapter<ImagesAdapter.ViewHo
                 placeholder(R.mipmap.ic_launcher)
                 transformations(CircleCropTransformation())
             }
-            title.text = "Image $position"
+            positionText.text = "Image $position"
+            author.text = version.author
         }
     }
 }
--- Lab_6_Levashova/app/src/main/java/com/example/lab_6_levashova/MainActivity.kt
+++ Lab_6_Levashova/app/src/main/java/com/example/lab_6_levashova/MainActivity.kt
@@ -2,18 +2,31 @@ package com.example.lab_6_levashova
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import android.widget.ProgressBar
+import androidx.appcompat.widget.SearchView
 import android.widget.Toast
+import androidx.core.view.MenuItemCompat
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity()
 {
     private val theImageAPIService = NetworkModule.theImageAPIService
+
+    private val mainImages = ArrayList<ImageResponse>()
+    private val imagesCopy = ArrayList<ImageResponse>()
 
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
     private lateinit var imagesList: RecyclerView
@@ -29,11 +42,106 @@ class MainActivity : AppCompatActivity()
         imagesList = findViewById(R.id.imageList)
         imagesList.setHasFixedSize(true)
 
-        imagesAdapter = ImagesAdapter(this)
+        imagesCopy.addAll(mainImages)
+
+        imagesAdapter = ImagesAdapter(this, imagesCopy)
         imagesList.adapter = imagesAdapter
 
         progressBar = findViewById(R.id.progress_bar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.images_menu, menu)
+        val menuItem = menu?.findItem(R.id.search_item)
+
+        if (menuItem != null){
+            val searchView = menuItem.actionView as SearchView
+            val editText = searchView.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
+            editText.hint = "Поиск"
+
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(p0: String?): Boolean {
+                    return true
+                }
+
+                override fun onQueryTextChange(p0: String?): Boolean {
+
+                    if (p0!!.isNotEmpty()) {
+                        Log.d("@@@@@@@@@@@@@@@@@@", "onQueryTextChange: " + 1)
+                        Log.d("@@@@@@@@@@@@@@@@@@", "onQueryTextChange1: " + p0)
+
+                        imagesCopy.clear()
+                        val search = p0.toLowerCase(Locale.getDefault())
+                        mainImages.forEach {
+
+                            if (it.author.toLowerCase(Locale.getDefault()).contains(search)) {
+                                imagesCopy.add(it)
+                            }
+                        }
+                        imagesList.adapter?.notifyDataSetChanged()
+                    } else {
+                        imagesCopy.clear()
+                        imagesCopy.addAll(mainImages)
+                        imagesList.adapter?.notifyDataSetChanged()
+                    }
+//                    }
+                    return true
+                }
+            })
+        }
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        val inflater: MenuInflater = menuInflater
+//        inflater.inflate(R.menu.images_menu, menu)
+//
+////        val searchItem: MenuItem = findViewById(R.id.search_item)
+////        val searchView: SearchView = searchItem?.actionView as SearchView
+//
+//        val menuItem = menu?.findItem(R.id.search_item)
+//        val searchView = MenuItemCompat.getActionView(menuItem) as SearchView
+//
+//        searchView.imeOptions = EditorInfo.IME_ACTION_DONE
+//
+//        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+//            override fun onQueryTextSubmit(p0: String?): Boolean {
+//                return false;
+//            }
+//
+//            override fun onQueryTextChange(p0: String?): Boolean {
+//                imagesAdapter.filter.filter(p0)
+//                return false
+//            }
+//
+//        })
+//        return true
+//    }
+
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        val inflater: MenuInflater = menuInflater
+//        inflater.inflate(R.menu.images_menu, menu)
+//
+//        val searchItem = menu.findItem(R.id.search_item)
+//        val searchView = searchItem.actionView as SearchView
+//        searchView.imeOptions = EditorInfo.IME_ACTION_DONE
+//        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+//            override fun onQueryTextSubmit(query: String): Boolean {
+//                return false
+//            }
+//
+//            override fun onQueryTextChange(newText: String): Boolean {
+//                imagesAdapter.getFilter().filter(newText)
+//                return false
+//            }
+//        })
+//        return true
+//    }
 
     override fun onResume() {
         super.onResume()
@@ -48,7 +156,10 @@ class MainActivity : AppCompatActivity()
                 ) {
                     val images: List<ImageResponse> = response.body() ?: emptyList()
                     progressBar.isVisible = false
+                    imagesAdapter.deleteImages()
                     imagesAdapter.addImages(images)
+                    mainImages.clear()
+                    mainImages.addAll(images)
                 }
 
                 override fun onFailure(call: Call<List<ImageResponse>>, t: Throwable) {
